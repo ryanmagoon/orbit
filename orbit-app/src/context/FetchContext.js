97e2c0029d4a97be7d7992f1e5bc3515d4668923
Ryan Magoon <mr.magoon5@gmail.com>
2023-02-05 00:51:03 -0500

    feat: add token to auth axios via interceptor

--- orbit-app/src/context/FetchContext.js
+++ orbit-app/src/context/FetchContext.js
@@ -1,23 +1,36 @@
-import React, { createContext } from 'react';
-import axios from 'axios';
+import React, { createContext, useContext } from 'react'
+import axios from 'axios'
 
-const FetchContext = createContext();
-const { Provider } = FetchContext;
+import { AuthContext } from './AuthContext'
+
+const FetchContext = createContext()
+const { Provider } = FetchContext
 
 const FetchProvider = ({ children }) => {
+  const authContext = useContext(AuthContext)
   const authAxios = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
-  });
+    baseURL: process.env.REACT_APP_API_URL,
+  })
+
+  authAxios.interceptors.request.use(
+    (config) => {
+      config.headers.Authorization = `Bearer ${authContext.authState.token}`
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
 
   return (
     <Provider
       value={{
-        authAxios
+        authAxios,
       }}
     >
       {children}
     </Provider>
-  );
-};
+  )
+}
 
-export { FetchContext, FetchProvider };
+export { FetchContext, FetchProvider }
